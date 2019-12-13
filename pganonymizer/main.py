4f8cee1b817c37d71c275e026523fd932240369a
Henning Kage <henning.kage@gmail.com>
2019-12-13 17:17:47 +0100

    Some testing

--- pganonymizer/main.py
+++ pganonymizer/main.py
@@ -1,10 +1,19 @@
 from __future__ import print_function
 
 import argparse
+import logging
 import psycopg2
 import yaml
 
 from constants import DATABASE_ARGS
+
+
+log = logging.getLogger(__name__)
+
+
+def truncate_table(cursor, name):
+    log.info('Truncating table %s', name)
+    cursor.execute('TRUNCATE TABLE {table}'.format(table=name))
 
 
 def main():
@@ -19,13 +28,25 @@ def main():
     args = parser.parse_args()
     print(args)
 
+    schema = yaml.load(open(args.schema), Loader=yaml.FullLoader)
+    truncate_tables = schema['truncate']
+
     pg_args = ({name: value for name, value in zip(DATABASE_ARGS, (args.dbname, args.user, args.password, args.host, args.port))})
 
     with psycopg2.connect(**pg_args) as connection:
-        print("TRUE")
+        connection.autocommit = True
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+            for table in cursor.fetchall():
+                #print(table)
+                #print('*' * 20)
+                cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' and table_name = %s", table)
+                #for column in cursor.fetchall():
+                #    print(column)
 
-    schema = yaml.load(open(args.schema), Loader=yaml.FullLoader)
-    print(schema)
+    
+            for table in truncate_tables:
+                truncate_table(cursor, table)
 
 
 if __name__ == '__main__':
